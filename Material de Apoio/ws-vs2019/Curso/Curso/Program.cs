6c3fa09141bde4626e66957fa1c9c4ab9f317233
David <davidroliveira@gmail.com>
2020-03-23 10:28:11 -0300

    Testando Tipos de variáveis

---
+++ Material de Apoio/ws-vs2019/Curso/Curso/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SByte x = 100;
+            //Console.WriteLine(x);
+
+            byte n1 = 255; //overflow
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L; //L écomendação pra mostrar que é long
+
+            n1++;
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+
+        }
+    }
+}
