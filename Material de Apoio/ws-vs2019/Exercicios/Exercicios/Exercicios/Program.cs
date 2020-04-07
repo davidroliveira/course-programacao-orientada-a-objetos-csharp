8de44b192a515ebc5dffe834d537ecce1ed05d4b
David <davidroliveira@gmail.com>
2020-04-07 19:26:38 -0300

    Aula 47 - Membros estáticos - PARTE 2

--- Material de Apoio/ws-vs2019/Exercicios/Exercicios/Exercicios/Program.cs
+++ Material de Apoio/ws-vs2019/Exercicios/Exercicios/Exercicios/Program.cs
@@ -7,16 +7,14 @@ namespace Exercicios
     {
         static void Main(string[] args)
         {
-            Calculadora calc = new Calculadora();
-
             Console.Write("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                         
-            Console.WriteLine("Circunferência: " + calc.Circunferencia(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Circunferência: " + Calculadora.Circunferencia(raio).ToString("F2", CultureInfo.InvariantCulture));
 
-            Console.WriteLine("Volume: " + calc.Volume(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + Calculadora.Volume(raio).ToString("F2", CultureInfo.InvariantCulture));
 
-            Console.WriteLine("Valor de PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
 
         }
 
