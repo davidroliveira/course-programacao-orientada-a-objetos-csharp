26d3072b1946577ef7a03f93b24425ef79b5b341
David <davidroliveira@gmail.com>
2020-04-07 19:25:07 -0300

    Aula 47 - Membros sem estáticos - PARTE 2

---
+++ Material de Apoio/ws-vs2019/Exercicios/Exercicios/Exercicios/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicios
+{
+    class Calculadora
+    {
+        public double Pi = 3.14;
+
+        public double Circunferencia(double raio)
+        {
+            return 2.0 * Pi * raio;
+        }
+
+        public double Volume(double raio)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(raio, 3);
+        }
+
+    }
+}
--- Material de Apoio/ws-vs2019/Exercicios/Exercicios/Exercicios/Program.cs
+++ Material de Apoio/ws-vs2019/Exercicios/Exercicios/Exercicios/Program.cs
@@ -5,29 +5,19 @@ namespace Exercicios
 {
     class Program
     {
-        static double Pi = 3.14;
         static void Main(string[] args)
         {
+            Calculadora calc = new Calculadora();
 
             Console.Write("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                         
-            Console.WriteLine("Circunferência: " + Circunferencia(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Circunferência: " + calc.Circunferencia(raio).ToString("F2", CultureInfo.InvariantCulture));
 
-            Console.WriteLine("Volume: " + Volume(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + calc.Volume(raio).ToString("F2", CultureInfo.InvariantCulture));
 
-            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
 
-        }
-
-        static double Circunferencia(double raio)
-        {
-            return 2.0 * Pi * raio;
-        }
-
-        static double Volume(double raio)
-        {
-            return 4.0 / 3.0 * Pi * Math.Pow(raio, 3);
         }
 
     }
