5d58285d5bee1e42b9bf50dcb50a2c4700c3e922
David <davidroliveira@gmail.com>
2020-04-08 19:47:55 -0300

    Aula 51 - Construtores

--- Material de Apoio/ws-vs2019/Exercicios/Exercicios/Exercicios/Program.cs
+++ Material de Apoio/ws-vs2019/Exercicios/Exercicios/Exercicios/Program.cs
@@ -7,14 +7,34 @@ namespace Exercicios
     {
         static void Main(string[] args)
         {
-            Console.Write("Entre o valor do raio: ");
-            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                        
-            Console.WriteLine("Circunferência: " + Calculadora.Circunferencia(raio).ToString("F2", CultureInfo.InvariantCulture));
 
-            Console.WriteLine("Volume: " + Calculadora.Volume(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
 
         }
 
