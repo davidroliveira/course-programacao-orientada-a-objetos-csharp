c9d0a5599029e07f98a0b574bb96cdde35a7c891
David <davidroliveira@gmail.com>
2020-04-14 19:16:55 -0300

    Aula 57 - Auto Properties

--- Material de Apoio/ws-vs2019/Exercicios/Exercicios/Exercicios/Produto.cs
+++ Material de Apoio/ws-vs2019/Exercicios/Exercicios/Exercicios/Produto.cs
@@ -7,30 +7,25 @@ namespace Exercicios
 {
     class Produto
     {
-
         private string _nome;
-        private double _preco;
-        private int _quantidade;
-               
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
         public Produto()
         {
         }
 
         public Produto(string nome, double preco, int quantidade)
         {
-            _nome = nome;
-            _preco = preco;
-            _quantidade = quantidade;
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
         }
 
         public string Nome
         {
-            get 
-            { 
-                return _nome; 
-            }
-
-            set 
+            get { return _nome; }
+            set
             {
                 if (value != null && value.Length > 1)
                 {
@@ -38,43 +33,26 @@ namespace Exercicios
                 }
             }
         }
-        
-        public double Preco
-        {
-            get
-            {
-                return _preco;
-            }
-        }
-
-
-        public int Quantidade
-        {
-            get
-            {
-                return _quantidade;
-            }
-        }
 
         public double ValorTotalEmEstoque()
         {
-            return _preco * _quantidade;
+            return Preco * Quantidade;
         }
         public void AdicionarProdutos(int quantidade)
         {
-            _quantidade += quantidade;
+            Quantidade += quantidade;
         }
         public void RemoverProdutos(int quantidade)
         {
-            _quantidade -= quantidade;
+            Quantidade -= quantidade;
         }
         public override string ToString()
         {
-            return _nome
+            return Nome
             + ", $ "
-            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
             + ", "
-            + _quantidade
+            + Quantidade
             + " unidades, Total: $ "
             + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
