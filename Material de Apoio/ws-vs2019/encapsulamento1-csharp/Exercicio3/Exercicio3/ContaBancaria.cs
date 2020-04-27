3a06e187182494c5cc89799784fe4851076987af
David <davidroliveira@gmail.com>
2020-04-27 20:24:08 -0300

    Aula 60 - Exercício de fixação

---
+++ Material de Apoio/ws-vs2019/encapsulamento1-csharp/Exercicio3/Exercicio3/ContaBancaria.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Exercicio3
+{
+    class ContaBancaria
+    {
+        private double _taxa = 5.00;
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0.00;
+        }
+
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= (quantia + _taxa);
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Numero + ", " +
+                   "Titular: " + Titular + ", " +
+                   "Saldo: $ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Material de Apoio/ws-vs2019/encapsulamento1-csharp/Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contaBancaria;
+
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial(s / n) ? ");
+            bool haveraDepositoInicial = Console.ReadLine().ToLower() == "s";           
+
+            if (haveraDepositoInicial)
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                contaBancaria = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                contaBancaria = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(contaBancaria);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double valorDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Deposito(valorDeposito);
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(contaBancaria);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            double valorSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Saque(valorSaque);
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(contaBancaria);
+
+        }
+    }
+}
