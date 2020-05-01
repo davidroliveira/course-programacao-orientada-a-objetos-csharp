600f1aa5bb6d5616c2c7987852aea4b4451c74f9
David <davidroliveira@gmail.com>
2020-05-01 18:38:23 -0300

    Aula 68 - Nullable (e operador de Operador de coalescência nula)

--- Material de Apoio/ws-vs2019/Exercicios/Exercicios/Exercicios/Program.cs
+++ Material de Apoio/ws-vs2019/Exercicios/Exercicios/Exercicios/Program.cs
@@ -7,15 +7,30 @@ namespace Exercicios
     {
         static void Main(string[] args)
         {
-            Point p;
-            p.X = 10;
-            p.Y = 20;
-            Console.WriteLine(p);
+            double? x = null;
+            double? y = 10.0;
 
-            p = new Point();
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
 
-            Console.WriteLine(p);
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
 
+            if (x.HasValue)
+                Console.WriteLine(x.Value);
+            else
+                Console.WriteLine("x is null");
+
+            if (y.HasValue)
+              Console.WriteLine(y.Value);
+            else
+                Console.WriteLine("y is null");
+
+            double a = x ?? 5;
+            double b = y ?? 5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
         }
 
     }
