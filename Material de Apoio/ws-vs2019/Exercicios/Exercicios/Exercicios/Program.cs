f9877a991674d494414e2dc4e2fbfa03533e5e0c
David <davidroliveira@gmail.com>
2020-05-05 19:15:23 -0300

    Aula 70 - Vetores - PARTE 2

--- Material de Apoio/ws-vs2019/Exercicios/Exercicios/Exercicios/Program.cs
+++ Material de Apoio/ws-vs2019/Exercicios/Exercicios/Exercicios/Program.cs
@@ -9,21 +9,22 @@ namespace Exercicios
         {
             int n = int.Parse(Console.ReadLine());
 
-            double[] vect = new double[n];
+            Produto[] produtos = new Produto[n];
 
             for (int i = 0; i < n; i++)
             {
-                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                produtos[i] = new Produto();
+                produtos[i].Nome = Console.ReadLine();
+                produtos[i].Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             }
 
             double sum = 0.0;
             for (int i = 0; i < n; i++)
             {
-                sum += vect[i];
+                sum += produtos[i].Preco;
             }
 
             double avg = sum / n;
-
             Console.WriteLine("AVERAGE HEIGHT = " + avg.ToString("F2", CultureInfo.InvariantCulture));
 
         }
